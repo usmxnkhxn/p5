b37a3aac24df478837fb5bf7c20c5a2bc841f079
Usman Khan <shierce.khan@gmail.com>
2024-11-14 22:49:17 -0600

    Added Brian's Code

---
+++ xv6-public/vm.c
@@ -0,0 +1,698 @@
+#include "param.h"
+#include "types.h"
+#include "defs.h"
+#include "x86.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "elf.h"
+#include "wmap.h"
+#include "spinlock.h"
+#include "fs.h"
+#include "sleeplock.h"
+#include "file.h"
+#include "stat.h"
+#include "fcntl.h"
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
+extern char data[];  // defined by kernel.ld
+pde_t *kpgdir;  // for use in scheduler()
+
+
+struct wmapinfo *wmapinfo;
+
+
+// Set up CPU's kernel segment descriptors.
+// Run once on entry on each CPU.
+void
+seginit(void)
+{
+  struct cpu *c;
+
+  // Map "logical" addresses to virtual addresses using identity map.
+  // Cannot share a CODE descriptor for both kernel and user
+  // because it would have to have DPL_USR, but the CPU forbids
+  // an interrupt from CPL=0 to DPL=3.
+  c = &cpus[cpuid()];
+  c->gdt[SEG_KCODE] = SEG(STA_X|STA_R, 0, 0xffffffff, 0);
+  c->gdt[SEG_KDATA] = SEG(STA_W, 0, 0xffffffff, 0);
+  c->gdt[SEG_UCODE] = SEG(STA_X|STA_R, 0, 0xffffffff, DPL_USER);
+  c->gdt[SEG_UDATA] = SEG(STA_W, 0, 0xffffffff, DPL_USER);
+  lgdt(c->gdt, sizeof(c->gdt));
+}
+
+// Return the address of the PTE in page table pgdir
+// that corresponds to virtual address va.  If alloc!=0,
+// create any required page table pages.
+static pte_t *
+walkpgdir(pde_t *pgdir, const void *va, int alloc)
+{
+  pde_t *pde;
+  pte_t *pgtab;
+
+  pde = &pgdir[PDX(va)];
+  if(*pde & PTE_P){
+    pgtab = (pte_t*)P2V(PTE_ADDR(*pde));
+  } else {
+    if(!alloc || (pgtab = (pte_t*)kalloc()) == 0)
+      return 0;
+    // Make sure all those PTE_P bits are zero.
+    memset(pgtab, 0, PGSIZE);
+    // The permissions here are overly generous, but they can
+    // be further restricted by the permissions in the page table
+    // entries, if necessary.
+    *pde = V2P(pgtab) | PTE_P | PTE_W | PTE_U;
+  }
+  return &pgtab[PTX(va)];
+}
+
+// Create PTEs for virtual addresses starting at va that refer to
+// physical addresses starting at pa. va and size might not
+// be page-aligned.
+int
+mappages(pde_t *pgdir, void *va, uint size, uint pa, int perm)
+{
+  char *a, *last;
+  pte_t *pte;
+
+  a = (char*)PGROUNDDOWN((uint)va);
+  last = (char*)PGROUNDDOWN(((uint)va) + size - 1);
+  for(;;){
+    if((pte = walkpgdir(pgdir, a, 1)) == 0)
+      return -1;
+    if(*pte & PTE_P)
+      panic("remap");
+    *pte = pa | perm | PTE_P;
+    if(a == last)
+      break;
+    a += PGSIZE;
+    pa += PGSIZE;
+  }
+  return 0;
+}
+
+// There is one page table per process, plus one that's used when
+// a CPU is not running any process (kpgdir). The kernel uses the
+// current process's page table during system calls and interrupts;
+// page protection bits prevent user code from using the kernel's
+// mappings.
+//
+// setupkvm() and exec() set up every page table like this:
+//
+//   0..KERNBASE: user memory (text+data+stack+heap), mapped to
+//                phys memory allocated by the kernel
+//   KERNBASE..KERNBASE+EXTMEM: mapped to 0..EXTMEM (for I/O space)
+//   KERNBASE+EXTMEM..data: mapped to EXTMEM..V2P(data)
+//                for the kernel's instructions and r/o data
+//   data..KERNBASE+PHYSTOP: mapped to V2P(data)..PHYSTOP,
+//                                  rw data + free physical memory
+//   0xfe000000..0: mapped direct (devices such as ioapic)
+//
+// The kernel allocates physical memory for its heap and for user memory
+// between V2P(end) and the end of physical memory (PHYSTOP)
+// (directly addressable from end..P2V(PHYSTOP)).
+
+// This table defines the kernel's mappings, which are present in
+// every process's page table.
+static struct kmap {
+  void *virt;
+  uint phys_start;
+  uint phys_end;
+  int perm;
+} kmap[] = {
+ { (void*)KERNBASE, 0,             EXTMEM,    PTE_W}, // I/O space
+ { (void*)KERNLINK, V2P(KERNLINK), V2P(data), 0},     // kern text+rodata
+ { (void*)data,     V2P(data),     PHYSTOP,   PTE_W}, // kern data+memory
+ { (void*)DEVSPACE, DEVSPACE,      0,         PTE_W}, // more devices
+};
+
+// Set up kernel part of a page table.
+pde_t*
+setupkvm(void)
+{
+  pde_t *pgdir;
+  struct kmap *k;
+
+  if((pgdir = (pde_t*)kalloc()) == 0)
+    return 0;
+  memset(pgdir, 0, PGSIZE);
+  if (P2V(PHYSTOP) > (void*)DEVSPACE)
+    panic("PHYSTOP too high");
+  for(k = kmap; k < &kmap[NELEM(kmap)]; k++)
+    if(mappages(pgdir, k->virt, k->phys_end - k->phys_start,
+                (uint)k->phys_start, k->perm) < 0) {
+      freevm(pgdir);
+      return 0;
+    }
+  return pgdir;
+}
+
+// Allocate one page table for the machine for the kernel address
+// space for scheduler processes.
+void
+kvmalloc(void)
+{
+  kpgdir = setupkvm();
+  switchkvm();
+}
+
+// Switch h/w page table register to the kernel-only page table,
+// for when no process is running.
+void
+switchkvm(void)
+{
+  lcr3(V2P(kpgdir));   // switch to the kernel page table
+}
+
+// Switch TSS and h/w page table to correspond to process p.
+void
+switchuvm(struct proc *p)
+{
+  if(p == 0)
+    panic("switchuvm: no process");
+  if(p->kstack == 0)
+    panic("switchuvm: no kstack");
+  if(p->pgdir == 0)
+    panic("switchuvm: no pgdir");
+
+  pushcli();
+  mycpu()->gdt[SEG_TSS] = SEG16(STS_T32A, &mycpu()->ts,
+                                sizeof(mycpu()->ts)-1, 0);
+  mycpu()->gdt[SEG_TSS].s = 0;
+  mycpu()->ts.ss0 = SEG_KDATA << 3;
+  mycpu()->ts.esp0 = (uint)p->kstack + KSTACKSIZE;
+  // setting IOPL=0 in eflags *and* iomb beyond the tss segment limit
+  // forbids I/O instructions (e.g., inb and outb) from user space
+  mycpu()->ts.iomb = (ushort) 0xFFFF;
+  ltr(SEG_TSS << 3);
+  lcr3(V2P(p->pgdir));  // switch to process's address space
+  popcli();
+}
+
+// Load the initcode into address 0 of pgdir.
+// sz must be less than a page.
+void
+inituvm(pde_t *pgdir, char *init, uint sz)
+{
+  char *mem;
+
+  if(sz >= PGSIZE)
+    panic("inituvm: more than a page");
+  mem = kalloc();
+  memset(mem, 0, PGSIZE);
+  mappages(pgdir, 0, PGSIZE, V2P(mem), PTE_W|PTE_U);
+  memmove(mem, init, sz);
+}
+
+// Load a program segment into pgdir.  addr must be page-aligned
+// and the pages from addr to addr+sz must already be mapped.
+int
+loaduvm(pde_t *pgdir, char *addr, struct inode *ip, uint offset, uint sz, int flags)
+{
+  uint i, pa, n;
+  pte_t *pte;
+
+  if((uint) addr % PGSIZE != 0)
+    panic("loaduvm: addr must be page aligned");
+  for(i = 0; i < sz; i += PGSIZE){
+    if((pte = walkpgdir(pgdir, addr+i, 0)) == 0)
+      panic("loaduvm: address should exist");
+    pa = PTE_ADDR(*pte);
+
+    if(sz - i < PGSIZE)
+      n = sz - i;
+    else
+      n = PGSIZE;
+    if(readi(ip, P2V(pa), offset+i, n) != n)
+      return -1;
+  
+	// task 3 ELF  
+    if(flags & ELF_PROG_FLAG_WRITE){
+      *pte |= PTE_W; // Add write permission if the segment is writable
+    } else {
+	  *pte &= ~PTE_W;
+    }
+  }
+  return 0;
+}
+
+// Allocate page tables and physical memory to grow process from oldsz to
+// newsz, which need not be page aligned.  Returns new size or 0 on error.
+int
+allocuvm(pde_t *pgdir, uint oldsz, uint newsz)
+{
+  char *mem;
+  uint a;
+
+  if(newsz >= KERNBASE)
+    return 0;
+  if(newsz < oldsz)
+    return oldsz;
+
+  a = PGROUNDUP(oldsz);
+  for(; a < newsz; a += PGSIZE){
+    mem = kalloc();
+    if(mem == 0){
+      cprintf("allocuvm out of memory\n");
+      deallocuvm(pgdir, newsz, oldsz);
+      return 0;
+    }
+    memset(mem, 0, PGSIZE);
+    if(mappages(pgdir, (char*)a, PGSIZE, V2P(mem), PTE_W|PTE_U) < 0){
+      cprintf("allocuvm out of memory (2)\n");
+      deallocuvm(pgdir, newsz, oldsz);
+      kfree(mem);
+      return 0;
+    }
+  }
+  return newsz;
+}
+
+// Deallocate user pages to bring the process size from oldsz to
+// newsz.  oldsz and newsz need not be page-aligned, nor does newsz
+// need to be less than oldsz.  oldsz can be larger than the actual
+// process size.  Returns the new process size.
+int
+deallocuvm(pde_t *pgdir, uint oldsz, uint newsz)
+{
+  pte_t *pte;
+  uint a, pa;
+
+  if(newsz >= oldsz)
+    return oldsz;
+
+  a = PGROUNDUP(newsz);
+  for(; a  < oldsz; a += PGSIZE){
+    pte = walkpgdir(pgdir, (char*)a, 0);
+    if(!pte)
+      a = PGADDR(PDX(a) + 1, 0, 0) - PGSIZE;
+    else if((*pte & PTE_P) != 0){
+      pa = PTE_ADDR(*pte);
+      if(pa == 0)
+        panic("kfree");
+      char *v = P2V(pa);
+      kfree(v);
+      *pte = 0;
+    }
+  }
+  return newsz;
+}
+
+// Free a page table and all the physical memory pages
+// in the user part.
+void
+freevm(pde_t *pgdir)
+{
+  uint i;
+
+  if(pgdir == 0)
+    panic("freevm: no pgdir");
+  deallocuvm(pgdir, KERNBASE, 0);
+  for(i = 0; i < NPDENTRIES; i++){
+    if(pgdir[i] & PTE_P){
+      char * v = P2V(PTE_ADDR(pgdir[i]));
+      kfree(v);
+    }
+  }
+  kfree((char*)pgdir);
+}
+
+// Clear PTE_U on a page. Used to create an inaccessible
+// page beneath the user stack.
+void
+clearpteu(pde_t *pgdir, char *uva)
+{
+  pte_t *pte;
+
+  pte = walkpgdir(pgdir, uva, 0);
+  if(pte == 0)
+    panic("clearpteu");
+  *pte &= ~PTE_U;
+}
+
+// Given a parent process's page table, create a copy
+// of it for a child.
+pde_t*
+copyuvm(pde_t *pgdir, uint sz)
+{
+  pde_t *d;
+  pte_t *pte;
+  uint pa, i, flags;
+  char *mem;
+
+  if((d = setupkvm()) == 0)
+    return 0;
+  for(i = 0; i < sz; i += PGSIZE){
+    if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
+      panic("copyuvm: pte should exist");
+    if(!(*pte & PTE_P))
+      panic("copyuvm: page not present");
+    pa = PTE_ADDR(*pte);
+    flags = PTE_FLAGS(*pte);
+    if((mem = kalloc()) == 0)
+      goto bad;
+    memmove(mem, (char*)P2V(pa), PGSIZE);
+    if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0) {
+      kfree(mem);
+      goto bad;
+    }
+  }
+  return d;
+
+bad:
+  freevm(d);
+  return 0;
+}
+
+//PAGEBREAK!
+// Map user virtual address to kernel address.
+char*
+uva2ka(pde_t *pgdir, char *uva)
+{
+  pte_t *pte;
+
+  pte = walkpgdir(pgdir, uva, 0);
+  if((*pte & PTE_P) == 0)
+    return 0;
+  if((*pte & PTE_U) == 0)
+    return 0;
+  return (char*)P2V(PTE_ADDR(*pte));
+}
+
+// Copy len bytes from p to user address va in page table pgdir.
+// Most useful when pgdir is not the current page table.
+// uva2ka ensures this only works for PTE_U pages.
+int
+copyout(pde_t *pgdir, uint va, void *p, uint len)
+{
+  char *buf, *pa0;
+  uint n, va0;
+
+  buf = (char*)p;
+  while(len > 0){
+    va0 = (uint)PGROUNDDOWN(va);
+    pa0 = uva2ka(pgdir, (char*)va0);
+    if(pa0 == 0)
+      return -1;
+    n = PGSIZE - (va - va0);
+    if(n > len)
+      n = len;
+    memmove(pa0 + (va - va0), buf, n);
+    len -= n;
+    buf += n;
+    va = va0 + PGSIZE;
+  }
+  return 0;
+}
+
+
+
+
+
+
+
+static int overlap(struct proc *curproc, uint address, int length)
+{
+  struct wmap_region mapping;
+  int i;
+  uint end = address + length;
+  for (i = 0; i < MAX_MAPPINGS; i++)
+  {
+    mapping = curproc->mappings[i];
+    if (mapping.addr && mapping.length)
+    {
+      uint mapping_start = mapping.addr;
+      uint mapping_end = mapping_start + mapping.length;
+      if (address >= mapping_start && address < mapping_end)
+      {
+        return 1;
+      }
+      if (end > mapping_start && end <= mapping_end)
+      {
+        return 1;
+      }
+      if (address <= mapping_start && end >= mapping_end)
+      {
+        return 1;
+      }
+    }
+  }
+  return 0;
+}
+
+uint
+wmap(uint addr, int length, int flags, int fd)
+{
+    struct proc *p = myproc();
+    struct file *f = 0;
+
+    // Validate flags
+    if (!(flags & MAP_FIXED)) {
+        cprintf("wmap: MAP_FIXED flag must be set\n");
+        return FAILED;
+    }
+
+    if (!(flags & MAP_SHARED)) {
+        cprintf("wmap: MAP_SHARED flag must be set\n");
+        return FAILED;
+    }
+
+    // Validate address
+    if (addr % PGSIZE != 0 || addr < 0x60000000 || addr + length > 0x80000000) {
+        cprintf("wmap: Invalid address range\n");
+        return FAILED;
+    }
+
+    // Validate length
+    if (length <= 0) {
+        cprintf("wmap: Length must be greater than 0\n");
+        return FAILED;
+    }
+
+    // Check for available mapping slots
+    if (p->wmap_count >= MAX_MAPPINGS) {
+        cprintf("wmap: Maximum number of mappings reached\n");
+        return FAILED;
+    }
+
+    // Handle file-backed mapping
+    if (!(flags & MAP_ANONYMOUS)) {
+        if (fd < 0 || fd >= NOFILE || !(f = p->ofile[fd])) {
+            cprintf("wmap: Invalid file descriptor\n");
+            return FAILED;
+        }
+        filedup(f); // Increase file reference count
+    }
+
+    // Check for memory mapping overlap
+    if ((flags & MAP_FIXED) && (overlap(p,addr,length))) {
+        cprintf("wmap: Memory mapping overlapped\n");
+        return FAILED;
+    }
+
+    // Register the mapping
+    int index = p->wmap_count++;
+    p->mappings[index].addr = addr;
+    p->mappings[index].length = length;
+    p->mappings[index].flags = flags;
+    p->mappings[index].fd = fd;
+    p->mappings[index].f = f;
+    // No physical memory allocation here (lazy allocation)
+    return addr;
+}
+
+static void
+unmap_region(pde_t *pgdir, uint addr, uint length)
+{
+    uint a;
+    for (a = addr; a < addr + length; a += PGSIZE) {
+        pte_t *pte = walkpgdir(pgdir, (void*)a, 0);
+        if (pte && (*pte & PTE_P)) {
+            uint pa = PTE_ADDR(*pte);
+            char *v = P2V(pa);
+            kfree(v);       // Free the physical memory
+            *pte = 0;       // Remove the PTE
+        }
+    }
+}
+
+
+static int 
+wr_2_file(struct file *f, uint va, int file_offset, int num_bytes) 
+{
+    int bytes_written = 0;     // Tracks the total number of bytes successfully written.
+    int max_chunk_size = ((MAXOPBLOCKS - 4) / 2) * 512;  // Calculate maximum bytes per write.
+
+    while (bytes_written < num_bytes) {
+        int remaining_bytes = num_bytes - bytes_written;
+        int chunk_size = remaining_bytes > max_chunk_size ? max_chunk_size : remaining_bytes;
+
+        begin_op();  // Start a file system operation.
+        ilock(f->ip);  // Lock the inode to ensure exclusive access.
+
+        // Attempt to write 'chunk_size' bytes from the virtual address offset.
+        int result = writei(f->ip, (char*) va + bytes_written, file_offset, chunk_size);
+        
+        iunlock(f->ip);  // Unlock the inode after writing.
+        end_op();  // Finish the file system operation.
+
+        if (result < 0) {
+            // If there is an error during write, exit the loop.
+            break;
+        }
+
+        if (result != chunk_size) {
+            // If written bytes do not match the intended bytes, report error and halt.
+            panic("short filewrite");
+        }
+
+        file_offset += result;  // Increment the file offset by the number of bytes written.
+        bytes_written += result;  // Increment the total bytes written counter.
+    }
+
+    // If total bytes written matches the number of bytes requested, return that number, otherwise return -1.
+    return (bytes_written == num_bytes) ? num_bytes : -1;
+}
+/*
+int wr_2_file(struct file *f, uint va, int offset, int n_bytes){
+	int r;
+	int max = ((MAXOPBLOCKS - 4) / 2) * 512;
+	int i = 0;
+	while(i < n_bytes){
+		int n1 = n_bytes - i;
+		if (n1 > max)
+			n1 = max;
+		begin_op();
+		ilock(f->ip);
+		if((r = writei(f->ip, (char*) va + i, offset, n1)) > 0)
+			offset += r;
+		iunlock(f->ip);
+		end_op();
+	
+		if (r < 0)
+			break;
+		if (r != n1)
+			panic("short filewrite\n"); 
+		i += r;
+
+	}
+	r = (i == n_bytes ? n_bytes : -1);
+	return r;
+}
+*/
+int
+wunmap(uint addr)
+{
+    struct proc *p = myproc();
+    int i;
+
+    // Check if addr is page-aligned
+    if (addr % PGSIZE != 0) {
+        cprintf("wunmap: Address is not page-aligned\n");
+        return FAILED;
+    }
+
+    // Find the mapping with the given addr
+    for (i = 0; i < p->wmap_count; i++) {
+        if (p->mappings[i].addr == addr) {
+            uint length = p->mappings[i].length;
+            uint start = addr;
+            uint end = addr + length;
+
+            // If it's a file-backed mapping with MAP_SHARED, write back to file
+            if (!(p->mappings[i].flags & MAP_ANONYMOUS) && (p->mappings[i].flags & MAP_SHARED)) {
+                struct file *f = p->mappings[i].f;
+                uint va;
+
+                for (va = start; va < end; va += PGSIZE) {
+                    pte_t *pte = walkpgdir(p->pgdir, (void*)va, 0);
+                    if (pte && (*pte & PTE_P)) {
+                        //char *mem = P2V(PTE_ADDR(*pte));
+                        int file_offset = va - start;
+                        int write_bytes = min(PGSIZE, end - va);
+						
+						if (wr_2_file(f,va,file_offset,write_bytes) == -1){
+                            cprintf("wunmap: writei failed\n");
+                            p->killed = 1;
+                            return FAILED;
+						}
+                    }
+                }
+            }
+
+			cprintf("2\n");
+            // Unmap pages and free memory
+            unmap_region(p->pgdir, start, length);
+
+            // Close the file if necessary
+            if (p->mappings[i].f) {
+                fileclose(p->mappings[i].f);
+                p->mappings[i].f = 0;
+            }
+
+
+			// Remove mapping from mappings array
+			int last = p->wmap_count - 1;
+
+			// Replace the mapping to be removed with the last mapping
+			p->mappings[i] = p->mappings[last];
+
+			// Update wmap_count
+			p->wmap_count--;
+	
+			// Also update wmapinfo arrays
+			p->mappings[i].addr = p->mappings[last].addr;
+			p->mappings[i].length = p->mappings[last].length;
+			p->mappings[i].n_loaded_pages = p->mappings[last].n_loaded_pages;
+            return SUCCESS;
+        }
+    }
+
+    // If mapping not found
+    cprintf("wunmap: Mapping not found for addr 0x%x\n", addr);
+    return FAILED;
+}
+
+
+uint
+va2pa(uint va)
+{
+  pde_t *pgdir;
+  pte_t *pte;
+  uint pa;
+
+  pgdir = myproc()->pgdir; // Get page directory of current process
+  pte = walkpgdir(pgdir, (char*)va, 0); // Retrieve the page table entry for 'va'
+  if(!pte || !(*pte & PTE_P))  // Check if the PTE exists and is marked as present
+    return -1; // Return -1 if the PTE is not present or does not exist
+  
+  pa = PTE_ADDR(*pte) | (va & 0xFFF); // Extract physical address from PTE and add offset
+  return pa;
+}
+
+
+int
+getwmapinfo(struct wmapinfo *info)
+{
+    struct proc *p = myproc();
+    int i;
+
+    info->total_mmaps = p->wmap_count;
+
+    for (i = 0; i < p->wmap_count; i++) {
+        info->addr[i] = p->mappings[i].addr;
+        info->length[i] = p->mappings[i].length;
+        info->n_loaded_pages[i] = p->mappings[i].n_loaded_pages;
+    }
+
+    return SUCCESS;
+}
+
+//PAGEBREAK!
+// Blank page.
+//PAGEBREAK!
+// Blank page.
+//PAGEBREAK!
+// Blank page.
+
---
+++ xv6-public/wmap.h
@@ -0,0 +1,18 @@
+// Flags for wmap
+#define MAP_SHARED 0x0002
+#define MAP_ANONYMOUS 0x0004
+#define MAP_FIXED 0x0008
+
+// When any system call fails, returns -1
+#define FAILED -1
+#define SUCCESS 0
+
+// for `getwmapinfo`
+#define MAX_WMMAP_INFO 16
+struct wmapinfo {
+    int total_mmaps;                    // Total number of wmap regions
+    int addr[MAX_WMMAP_INFO];           // Starting address of mapping
+    int length[MAX_WMMAP_INFO];         // Size of mapping
+    int n_loaded_pages[MAX_WMMAP_INFO]; // Number of pages physically loaded into memory
+};
+
